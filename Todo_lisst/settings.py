cab5fe4be8217ff62151e6e494ea31beeefb19d5
Alireza Mortezaei <mortezaei_alireza@yahoo.com>
2021-04-07 18:36:43 +0430

    Final Todo App

--- Todo_lisst/settings.py
+++ Todo_lisst/settings.py
@@ -119,7 +119,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-LOGIN_URL = '/login/'
+LOGIN_URL = ''
 
 
 # Static files (CSS, JavaScript, Images)
--- base/urls.py
+++ base/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('create/', TaskCreate.as_view(), name='task_create'),
     path('update/<int:pk>/', TaskUpdate.as_view(), name='task_update'),
     path('delete/<int:pk>/', TaskDelete.as_view(), name='task_delete'),
-    path('login/', LoginUser.as_view(), name='login'),
+    path('', LoginUser.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page=reverse_lazy('base:login')), name='logout'),
     path('register/', UserCreation.as_view(), name='register'),
 ]
